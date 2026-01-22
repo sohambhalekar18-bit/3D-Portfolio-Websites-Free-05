8df2832ca6754580cbbf5d9b0fb7381907c0c077
Soham Bhalekar <sohambhalekar18@gmail.com>
2026-01-22 09:11:40 +0530

    Remove OTHER_SKILL from Skills component
    Removed the OTHER_SKILL section from the Skills component.

--- components/main/skills.tsx
+++ components/main/skills.tsx
@@ -5,7 +5,6 @@ import {
   BACKEND_SKILL,
   FRONTEND_SKILL,
   FULLSTACK_SKILL,
-  OTHER_SKILL,
   SKILL_DATA,
 } from "@/constants";
 
@@ -18,6 +17,7 @@ export const Skills = () => {
     >
       <SkillText />
 
+      {/* MAIN SKILLS */}
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {SKILL_DATA.map((skill, i) => (
           <SkillDataProvider
@@ -31,6 +31,7 @@ export const Skills = () => {
         ))}
       </div>
 
+      {/* FRONTEND SKILLS */}
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {FRONTEND_SKILL.map((skill, i) => (
           <SkillDataProvider
@@ -43,6 +44,8 @@ export const Skills = () => {
           />
         ))}
       </div>
+
+      {/* BACKEND SKILLS */}
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {BACKEND_SKILL.map((skill, i) => (
           <SkillDataProvider
@@ -55,6 +58,8 @@ export const Skills = () => {
           />
         ))}
       </div>
+
+      {/* FULLSTACK / TOOLS */}
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {FULLSTACK_SKILL.map((skill, i) => (
           <SkillDataProvider
@@ -67,19 +72,8 @@ export const Skills = () => {
           />
         ))}
       </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {OTHER_SKILL.map((skill, i) => (
-          <SkillDataProvider
-            key={skill.skill_name}
-            src={skill.image}
-            name={skill.skill_name}
-            width={skill.width}
-            height={skill.height}
-            index={i}
-          />
-        ))}
-      </div>
 
+      {/* BACKGROUND VIDEO */}
       <div className="w-full h-full absolute">
         <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
           <video
